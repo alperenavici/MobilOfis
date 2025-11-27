1d72fe94de6c9ba80ea7bd0798a3da23e425ebe4
Alperen Avci <alperenavci49@gmail.com>
2025-11-27 15:54:30 +0300

    feat: modernize header user profile UI by refactoring HTML structure and adding dedicated CSS styles.

--- MobilOfis.Data/obj/Debug/net8.0/MobilOfis.Data.AssemblyInfo.cs
+++ MobilOfis.Data/obj/Debug/net8.0/MobilOfis.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobilOfis.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4142f1ec93dd23f10c65fc5fbc8715d3a5ed67c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14dc3fcb67b1107edb305fac35a2cd33f689c14f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobilOfis.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobilOfis.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
