60a8adb3f443fe55dc36917c12dadffd5cb74c68
Alperen Avci <alperenavci49@gmail.com>
2025-11-29 15:58:33 +0300

    feat: Enhance user list filtering by updating parameters, adding department data, and preserving filter selections in the view.

--- MobilOfis.Web/obj/Debug/net8.0/MobilOfis.Web.AssemblyInfo.cs
+++ MobilOfis.Web/obj/Debug/net8.0/MobilOfis.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobilOfis.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a85643ff0992f55dc2ca5b94b2f612a5356b57d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+649d0530b58b324398b9a9b48601bab28d13cf23")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobilOfis.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobilOfis.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
