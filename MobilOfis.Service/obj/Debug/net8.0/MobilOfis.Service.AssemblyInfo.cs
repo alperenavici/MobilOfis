884ee87e862b3a2b6dafc2237ced37db86716b2b
Alperen Avci <alperenavci49@gmail.com>
2025-11-29 13:31:05 +0300

    style: Refine badge styling with new components.css and remove redundant comments from LeaveController.

--- MobilOfis.Service/obj/Debug/net8.0/MobilOfis.Service.AssemblyInfo.cs
+++ MobilOfis.Service/obj/Debug/net8.0/MobilOfis.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobilOfis.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+665852d98e116b6a9486532950be4a0f9eac43d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed0e7e0dbcd6a0f47d9074d5582070b77f2d163")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobilOfis.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobilOfis.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
